8fb8abd5fe54d4a7753705040b816dfadb4e5633
404-bug-notfound <iqbal.jamadar@gmail.com>
2021-04-05 19:05:33 -0400

    Initial project setup

---
+++ src/main/java/com/galvanize/guestbookserviceapi/GuestbookServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.guestbookserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestbookServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestbookServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/guestbookserviceapi/GuestbookServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.guestbookserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestbookServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
