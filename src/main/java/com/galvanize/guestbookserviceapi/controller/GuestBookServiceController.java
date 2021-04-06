0ed9caad7fa09721389d70ab1bbb2aed19e24397
404-bug-notfound <iqbal.jamadar@gmail.com>
2021-04-06 19:39:58 -0400

    Refactor - moved controller class to separate controller package

---
+++ src/main/java/com/galvanize/guestbookserviceapi/controller/GuestBookServiceController.java
@@ -0,0 +1,37 @@
+package com.galvanize.guestbookserviceapi.controller;
+
+import com.galvanize.guestbookserviceapi.dto.GuestBookDto;
+import com.galvanize.guestbookserviceapi.service.GuestBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("guestbook")
+public class GuestBookServiceController {
+
+    @Autowired
+    private GuestBookService guestBookService;
+
+    /*@GetMapping
+    public String getAllGuestBookEntries(){
+        return "{}";
+    }*/
+
+    @GetMapping
+    public List<GuestBookDto> getAllGuestBookEntries(){
+        return guestBookService.getAllGuestBookEntries();
+    }
+
+    @PostMapping("comment")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addNewGuestBookComment(@RequestBody GuestBookDto guestBookDto) throws Exception{
+
+        guestBookService.addComment(guestBookDto);
+
+    }
+
+
+}
