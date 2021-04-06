b39fcd11cea5a23ab63b52cdbdbc682aea8ef3b2
404-bug-notfound <iqbal.jamadar@gmail.com>
2021-04-06 19:38:08 -0400

    Posting new guest book comment - Test Successful

--- src/test/java/com/galvanize/guestbookserviceapi/GuestBookServiceIT.java
+++ src/test/java/com/galvanize/guestbookserviceapi/GuestBookServiceIT.java
@@ -1,10 +1,12 @@
 package com.galvanize.guestbookserviceapi;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.galvanize.guestbookserviceapi.dto.GuestBookDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -26,15 +28,31 @@ public class GuestBookServiceIT {
     ObjectMapper objectMapper;
 
     @Test
-    void getAllGuestBookComments() throws Exception{
+    void getAllGuestBookCommentsTest() throws Exception{
 
         mockMvc.perform(
                 get("/guestbook"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("length()").value(0))
                 .andDo(print());
+    }
 
+    @Test
+    void putNewGuestBookCommentTest() throws Exception{
 
+        GuestBookDto input1 = new GuestBookDto("Iqbal", "First GuestBook Comment");
 
+        mockMvc.perform(
+                post("/guestbook/comment")
+                .content(objectMapper.writeValueAsString(input1))
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(
+                get("/guestbook"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("length()").value(1))
+                .andDo(print());
     }
 }
